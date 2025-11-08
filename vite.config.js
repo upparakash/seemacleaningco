f903e96f2bb421c59e4509f22c9a28184467ca22
uppar akash <akashuppar1998@gmail.com>
2025-11-08 20:21:21 +0530

    deploy line added

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
+    
   ],
 })
